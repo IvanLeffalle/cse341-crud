ac93e242afd7adc7828da3e95c22a581667ce0b4
IvanLeffalle <ivanmleffalle@gmail.com>
2025-05-30 21:30:37 -0300

    updated swagger file

--- swagger.js
+++ swagger.js
@@ -1,15 +1,15 @@
-const swaggerAutogen = require ('swagger-autogen')
+const swaggerAutogen = require("swagger-autogen");
 
 const doc = {
-    info: {
-        title: 'Tooth Lengths API',
-        description: 'API for managing approximate tooth lengths',
-    },
-    host: 'localhost:3000',
-    schemes: ['http'],
-}
+  info: {
+    title: "Tooth Lengths API",
+    description: "API for managing approximate tooth lengths",
+  },
+  host: "cse341-crud-jg27.onrender.com",
+  schemes: ["https"],
+};
 
-const outputFile = './swagger_output.json';
-const endpointsFiles = ['./routes/index.js'];
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["./routes/index.js"];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
